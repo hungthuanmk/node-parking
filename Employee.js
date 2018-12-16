292aae4c7ebfaba2232d5fa4bf533fa2cbbc3326
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2018-12-16 16:29:02 +0700

    Hello Coffee

--- Employee.js
+++ Employee.js
@@ -1,12 +1,25 @@
 const db = require('./Database.js');
+const jwt = require('jsonwebtoken');
 
 class Employee {
     static authentication(username, password, callback) {
        let sql = "SELECT cncparking.authentication('"+username+"', '"+password+"') AS name;";
        db.query(sql, (err, rows) => {
-           console.log(rows[0].name);
-           callback(rows[0].name);
+           let fName = rows[0].name;
+           if (fName) {
+               // callback(rows[0].name);
+               callback({token: 'JWT ' + jwt.sign({name: rows[0].name}, 'RESTFULAPIs')});
+           } else
+               callback("LOGIN FAILED");
        });
+    }
+
+    static loginRequired(req, res, next) {
+        if (req.user) {
+            next();
+        } else {
+            return res.status(401).json({message: 'Unauthorized user!'});
+        }
     }
 }
 
--- index.js
+++ index.js
@@ -2,14 +2,34 @@ const Database = require('./Database.js');
 const Employee = require('./Employee.js');
 const Report = require('./Report.js');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const bodyParser = require('body-parser');
+const jsonwebtoken = require('jsonwebtoken');
 
 const expressPort = 3000;
 
 app.set('view engine', 'ejs');
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// MIDDLEWARE
+app.use((req, res, next) => {
+    // let token = ;
+    // console.log('TOKEN => ' +  req.cookies.key.token);
+    if (req.cookies.key && req.cookies.key.token && req.cookies.key.token.split(' ')[0] === 'JWT') {
+        jsonwebtoken.verify(req.cookies.key.token.split(' ')[1], 'RESTFULAPIs', function(err, decode) {
+            if (err)
+                req.user = undefined;
+            req.user = decode;
+            next();
+        });
+    } else {
+        req.user = undefined;
+        next();
+    }
+});
 
 app.get('/', (req, res) => {
     // res.render('pages/login.ejs');
@@ -20,8 +40,21 @@ app.post('/login', (req, res) => {
     console.log(req.body.username);
     console.log(req.body.password);
     Employee.authentication(req.body.username, req.body.password, (employeeName) => {
+        //res.clearCookie('key');
+        res.cookie('key', employeeName, {expires: new Date(Date.now() + 1000*3600*12)});
         res.send(employeeName);
     });
+});
+
+app.get('/logout', (req, res) => {
+   res.clearCookie('key');
+   res.end();
+});
+
+app.get('/api/test', (req, res) => {
+   Employee.loginRequired(req, res, () => {
+       res.send('Hello World ' + req.user.name);
+   })
 });
 
 app.listen(expressPort, (err) => {
