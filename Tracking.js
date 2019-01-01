df48e0bdb6efd081d80b61333262d7dcd7df8688
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2019-01-01 22:53:34 +0700

    Update test

--- Tracking.js
+++ Tracking.js
@@ -8,77 +8,93 @@ module.exports = function (app) {
     };
 
     app.post('/api/Tracking/new', User.requireAuthentication, (req, res) => {
-        if (isNewValid(req.body.rfid))
-            DB.instance().table('Tracking').insert({rfid: req.body.rfid, in_userId: 1})
-                .catch((err) => {
-                    // console.log(err);
-                    res.status(400).json({errno: err.errno, sqlMessage: err.sqlMessage});
-                })
-                .then((d) => {
-                    if (d)
-                        res.status(200).json({trackingId: d[0]});
-                    else
-                        res.sendStatus(400);
-                });
-        else
+        if (!req.body || !req.body.rfid) {
             res.sendStatus(400);
+        } else {
+            if (isNewValid(req.body.rfid))
+                DB.instance().table('Tracking').insert({rfid: req.body.rfid, in_userId: 1})
+                    .catch((err) => {
+                        // console.log(err);
+                        res.status(400).json({errno: err.errno, sqlMessage: err.sqlMessage});
+                    })
+                    .then((d) => {
+                        if (d)
+                            res.status(200).json({trackingId: d[0]});
+                        else
+                            res.sendStatus(400);
+                    });
+            else
+                res.sendStatus(400);
+        }
     });
 
     app.put('/api/Tracking', User.requireAuthentication, (req, res) => {
         // {trackingid: int, type: [in(out)frontimg, in(out)plateimg, platenumber], data}
-        var jsonData = {};
-        switch (req.body.type) {
-            case 'infrontimg':
-                jsonData = {in_img_front: req.body.data};
-                break;
-            case 'outfrontimg':
-                jsonData = {out_img_front: req.body.data};
-                break;
-            case 'inplateimg':
-                jsonData = {in_img_plate: req.body.data};
-                break;
-            case 'outplateimg':
-                jsonData = {out_img_plate: req.body.data};
-                break;
-            case 'platenumber':
-                jsonData = {plateNumber: req.body.data};
-                break;
-            default:
-                res.sendStatus(400);
-        }
-        DB.instance().table('Tracking').where({trackingId: req.body.trackingId}).update(jsonData)
-            .then((ok) => {
-                if (ok)
-                    res.sendStatus(200);
-                else
+        if (!req.body || !req.body.type || !req.body.trackingId || !req.body.data) {
+            res.sendStatus(400);
+        } else {
+            var jsonData = {};
+            switch (req.body.type) {
+                case 'infrontimg':
+                    jsonData = {in_img_front: req.body.data};
+                    break;
+                case 'outfrontimg':
+                    jsonData = {out_img_front: req.body.data};
+                    break;
+                case 'inplateimg':
+                    jsonData = {in_img_plate: req.body.data};
+                    break;
+                case 'outplateimg':
+                    jsonData = {out_img_plate: req.body.data};
+                    break;
+                case 'platenumber':
+                    jsonData = {plateNumber: req.body.data};
+                    break;
+                default:
                     res.sendStatus(400);
-            })
+            }
+            DB.instance().table('Tracking').where({trackingId: req.body.trackingId}).update(jsonData)
+                .then((ok) => {
+                    if (ok)
+                        res.sendStatus(200);
+                    else
+                        res.sendStatus(400);
+                });
+        }
     });
 
     app.delete('/api/Tracking', User.requireAuthentication, (req, res) => {
-        DB.instance().table('Tracking').where({trackingId: req.body.trackingId}).del()
-            .then((ok) => {
-                if (ok)
-                    res.sendStatus(200);
-                else
-                    res.sendStatus(400); //BAD REQUEST
-            });
+        if (!req.body || !req.body.trackingId) {
+            res.sendStatus(400);
+        } else {
+            DB.instance().table('Tracking').where({trackingId: req.body.trackingId}).del()
+                .then((ok) => {
+                    if (ok)
+                        res.sendStatus(200);
+                    else
+                        res.sendStatus(400); //BAD REQUEST
+                });
+        }
     });
 
     app.get('/api/Tracking', User.requireAuthentication, (req, res) => {
-        DB.instance().table('Tracking').select('trackingId', 'type', 'plateNumber', 'in_timestamp', 'in_img_front', 'in_img_plate').where({rfid: req.body.rfid})
-            .then((data) => {
-                tracking = data[0];
-                if (tracking) {
-                    tracking.price = 3.5;
-                    // console.log(tracking);
-                    res.json(tracking);
-                } else
-                    res.sendStatus(400);
-            });
+        if (!req.body || !req.body.rfid) {
+            res.sendStatus(400);
+        } else {
+            DB.instance().table('Tracking').select('trackingId', 'type', 'plateNumber', 'in_timestamp', 'in_img_front', 'in_img_plate').where({rfid: req.body.rfid})
+                .then((data) => {
+                    tracking = data[0];
+                    if (tracking) {
+                        tracking.price = 3.5;
+                        // console.log(tracking);
+                        res.json(tracking);
+                    } else
+                        res.sendStatus(400);
+                });
+        }
     });
-}
-;
+
+};
 
 
 
